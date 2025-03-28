890b3188ac0245eeede8af7bcf71f7def02868bc
KingsDevs <sunnyckair@gmail.com>
2025-03-28 13:14:35 +0800

    Fix LiDAR transform parameters and update EKF configuration for improved localization

--- src/couliglig/launch/robot_launch.py
+++ src/couliglig/launch/robot_launch.py
@@ -69,7 +69,7 @@ def generate_launch_description():
     base_link_to_lidar = Node(
         package='tf2_ros',
         executable='static_transform_publisher',
-        arguments=['0', '0', '0.18', '0', '0', '0', 'base_link', 'LDS-01'],
+        arguments=['0', '0', '0', '0', '0', '0', 'base_link', 'LDS-01'],
         parameters=[{'use_sim_time': use_sim_time}]
     )
 
@@ -106,7 +106,7 @@ def generate_launch_description():
         # odom_publisher,
         # imu_publisher,
         # odom_to_baselink,
-        # base_link_to_lidar,
+        base_link_to_lidar,
         # joint_state_publisher,
         # joint_state_publisher_gui,
         robot_state_publisher,
